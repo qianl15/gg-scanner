02f7fd0010b8f8ef65503b9d08f63e2e8ee838f7
Qian Li <liqian.cs@gmail.com>
2018-04-26 05:44:42 +0000

    Update run_scanner_tf.sh, now we can pass num instances and decode batch as options

--- scripts/run_scanner_tf.sh
+++ scripts/run_scanner_tf.sh
@@ -4,8 +4,16 @@ SCRIPT=$HOME/gg-scanner/scanner_tf_label_image/main.py
 VIDEO=$HOME/4kvid_chunk6kf.mp4
 LOGFILE=scanner_tf.log
 
-numInstances=14
+numInstances=1
 decodeBatch=75
+
+# We can define num instances and decode batch in command line
+if [[ "$#" -gt 0 ]]; then
+    numInstances=$1
+    if [[ "$#" -gt 1 ]]; then
+        decodeBatch=$2
+    fi
+fi
 
 cmd="python $SCRIPT -n $numInstances -b $decodeBatch $VIDEO"
 echo $cmd
